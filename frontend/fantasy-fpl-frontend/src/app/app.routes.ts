a6c5cc6f30b7e03760087f5391e4f0d929544a0f
alshumb <alishumba61@gmail.com>
2026-01-27 10:30:40 +0200

    done with frontend changes

--- frontend/fantasy-fpl-frontend/src/app/app.routes.ts
+++ frontend/fantasy-fpl-frontend/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { TableComponent } from './components/table/table.component';
 import { InjuriesComponent } from './components/injuries/injuries.component';
 import { ClubsComponent } from './components/clubs/clubs.component';
 import { ClubDetailComponent } from './components/club-detail/club-detail.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { HomeComponent } from './components/home/home.component'; // Import Home
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/matches', pathMatch: 'full' },
+  // CHANGE THIS LINE: Redirect '' to 'home' (or component: HomeComponent directly)
+  { path: '', component: HomeComponent }, 
+  
   { path: 'matches', component: MatchesComponent },
   { path: 'table', component: TableComponent },
   { path: 'injuries', component: InjuriesComponent },
   { path: 'clubs', component: ClubsComponent },
-  { path: 'clubs/:id', component: ClubDetailComponent }, // Dynamic club page
+  { path: 'clubs/:id', component: ClubDetailComponent },
+  { path: 'statistics', component: StatisticsComponent }
 ];
